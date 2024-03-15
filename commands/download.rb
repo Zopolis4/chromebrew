32120ed2228a97042a8525e302f4430020795493
Zopolis4 <creatorsmithmdt@gmail.com>
2024-03-15 12:11:13 +1100

    oh yeah that

--- commands/download.rb
+++ commands/download.rb
@@ -53,89 +53,89 @@ class Command
       return filename, extract_dir
     end
   end
+end
 
-  def url_download(name, url, sha256sum, verbose)
-    downloader url, sha256sum, verbose
-    puts "#{name.capitalize} archive downloaded.".lightgreen
+def url_download(name, url, sha256sum, verbose)
+  downloader url, sha256sum, verbose
+  puts "#{name.capitalize} archive downloaded.".lightgreen
+end
+
+def git_download(pkg, extract_dir)
+  Dir.chdir extract_dir do
+    system 'git init'
+    system 'git config advice.detachedHead false'
+    system "git fetch --depth 1 #{pkg.source_url} #{pkg.git_hashtag}"
+    system 'git checkout FETCH_HEAD'
+    system 'git submodule update --init --recursive --depth 1'
+    system "git fetch --tags --depth 1 #{pkg.source_url}" if pkg.git_fetchtags?
+    puts 'Repository downloaded.'.lightgreen
   end
+end
 
-  def git_download(pkg, extract_dir)
-    Dir.chdir extract_dir do
-      system 'git init'
-      system 'git config advice.detachedHead false'
-      system "git fetch --depth 1 #{pkg.source_url} #{pkg.git_hashtag}"
-      system 'git checkout FETCH_HEAD'
-      system 'git submodule update --init --recursive --depth 1'
-      system "git fetch --tags --depth 1 #{pkg.source_url}" if pkg.git_fetchtags?
-      puts 'Repository downloaded.'.lightgreen
-   end
+def find_cached_url_download(name, filename, sha256sum, verbose)
+  puts "Looking for #{name} archive in cache".orange if verbose
+  # Privilege CREW_LOCAL_BUILD_DIR over CREW_CACHE_DIR.
+  cachefile = File.join(CREW_CACHE_DIR, filename) if File.file?(cachefile)
+  cachefile = File.join(CREW_LOCAL_BUILD_DIR, filename) if File.file?(cachefile)
+  if cachefile == File.join(CREW_LOCAL_BUILD_DIR, filename)
+    puts "Using #{name} archive from the build cache at #{cachefile}".orange
+    puts 'The checksum will not be checked against the package file.'.orange
   end
-
-  def find_cached_url_download(name, filename, sha256sum, verbose)
-    puts "Looking for #{name} archive in cache".orange if verbose
-    # Privilege CREW_LOCAL_BUILD_DIR over CREW_CACHE_DIR.
-    cachefile = File.join(CREW_CACHE_DIR, filename) if File.file?(cachefile)
-    cachefile = File.join(CREW_LOCAL_BUILD_DIR, filename) if File.file?(cachefile)
-    if cachefile == File.join(CREW_LOCAL_BUILD_DIR, filename)
-      puts "Using #{name} archive from the build cache at #{cachefile}".orange
-      puts 'The checksum will not be checked against the package file.'.orange
-    end
-    if cachefile
-      puts "#{name.capitalize} archive file exists in cache".lightgreen if verbose
-      # Don't check checksum if file is in the build cache.
-      if Digest::SHA256.hexdigest(File.read(cachefile)) == sha256sum || cachefile == File.join(CREW_LOCAL_BUILD_DIR, filename)
-        begin
-          # Hard link cached file if possible.
-          FileUtils.ln cachefile, CREW_BREW_DIR, force: true, verbose: verbose
-          puts 'Archive hard linked from cache'.green if verbose
-        rescue StandardError
-          # Copy cached file if hard link fails.
-          FileUtils.cp cachefile, CREW_BREW_DIR, verbose: verbose
-          puts 'Archive copied from cache'.green if verbose
-        end
-        puts 'Archive found in cache'.lightgreen
-      else
-        puts 'Cached archive checksum mismatch. 😔 Will download.'.lightred
+  if cachefile
+    puts "#{name.capitalize} archive file exists in cache".lightgreen if verbose
+    # Don't check checksum if file is in the build cache.
+    if Digest::SHA256.hexdigest(File.read(cachefile)) == sha256sum || cachefile == File.join(CREW_LOCAL_BUILD_DIR, filename)
+      begin
+        # Hard link cached file if possible.
+        FileUtils.ln cachefile, CREW_BREW_DIR, force: true, verbose: verbose
+        puts 'Archive hard linked from cache'.green if verbose
+      rescue StandardError
+        # Copy cached file if hard link fails.
+        FileUtils.cp cachefile, CREW_BREW_DIR, verbose: verbose
+        puts 'Archive copied from cache'.green if verbose
       end
+      puts 'Archive found in cache'.lightgreen
     else
-      puts 'Cannot find cached archive. 😔 Will download.'.lightred
+      puts 'Cached archive checksum mismatch. 😔 Will download.'.lightred
     end
+  else
+    puts 'Cannot find cached archive. 😔 Will download.'.lightred
   end
+end
 
-  def find_cached_git_download(pkg, extract_dir, verbose)
-    cachefile = File.join(CREW_CACHE_DIR, "#{filename}_#{pkg.git_hashtag.gsub('/', '_')}.tar.zst")
-    puts "Git cachefile is #{cachefile}".orange if verbose
-    if File.file?(cachefile) && File.file?("#{cachefile}.sha256")
-      if Digest::SHA256.hexdigest(File.read(cachefile)) == File.read("#{cachefile}.sha256")
-        system "tar -Izstd -x#{verbose}f #{cachefile} -C #{extract_dir}"
-      else
-        puts 'Cached git repository checksum mismatch. 😔 Will download.'.lightred
-      end
+def find_cached_git_download(pkg, extract_dir, verbose)
+  cachefile = File.join(CREW_CACHE_DIR, "#{filename}_#{pkg.git_hashtag.gsub('/', '_')}.tar.zst")
+  puts "Git cachefile is #{cachefile}".orange if verbose
+  if File.file?(cachefile) && File.file?("#{cachefile}.sha256")
+    if Digest::SHA256.hexdigest(File.read(cachefile)) == File.read("#{cachefile}.sha256")
+      system "tar -Izstd -x#{verbose}f #{cachefile} -C #{extract_dir}"
     else
-      puts 'Cannot find cached git repository. 😔 Will download.'.lightred
+      puts 'Cached git repository checksum mismatch. 😔 Will download.'.lightred
     end
+  else
+    puts 'Cannot find cached git repository. 😔 Will download.'.lightred
   end
+end
 
-  def cache_downloaded_file(filename, verbose)
-    begin
-      # Hard link to cache if possible.
-      FileUtils.ln filename, CREW_CACHE_DIR, verbose: verbose
-      puts 'Archive hard linked to cache'.green if verbose
-    rescue StandardError
-      # Copy to cache if hard link fails.
-      FileUtils.cp filename, CREW_CACHE_DIR, verbose: verbose
-      puts 'Archive copied to cache'.green if verbose
-    end
+def cache_downloaded_file(filename, verbose)
+  begin
+    # Hard link to cache if possible.
+    FileUtils.ln filename, CREW_CACHE_DIR, verbose: verbose
+    puts 'Archive hard linked to cache'.green if verbose
+  rescue StandardError
+    # Copy to cache if hard link fails.
+    FileUtils.cp filename, CREW_CACHE_DIR, verbose: verbose
+    puts 'Archive copied to cache'.green if verbose
   end
+end
 
-  def cache_git_dir(pkg, extract_dir, verbose)
-    cachefile = File.join(CREW_CACHE_DIR, "#{filename}_#{pkg.git_hashtag.gsub('/', '_')}.tar.zst")
-    puts 'Caching downloaded git repo...'
-    Dir.chdir extract_dir do
-      # Do not use --exclude-vcs to exclude .git because some builds will use that information.
-      system "tar c#{verbose} $(find -mindepth 1 -maxdepth 1 -printf '%P\n') | nice -n 20 #{CREW_PREFIX}/bin/zstd -c -T0 --ultra -20 - >  #{cachefile}"
-    end
-    File.write("#{cachefile}.sha256", Digest::SHA256.hexdigest(File.read(cachefile)))
-    puts 'Git repo cached.'.lightgreen
+def cache_git_dir(pkg, extract_dir, verbose)
+  cachefile = File.join(CREW_CACHE_DIR, "#{filename}_#{pkg.git_hashtag.gsub('/', '_')}.tar.zst")
+  puts 'Caching downloaded git repo...'
+  Dir.chdir extract_dir do
+    # Do not use --exclude-vcs to exclude .git because some builds will use that information.
+    system "tar c#{verbose} $(find -mindepth 1 -maxdepth 1 -printf '%P\n') | nice -n 20 #{CREW_PREFIX}/bin/zstd -c -T0 --ultra -20 - >  #{cachefile}"
   end
+  File.write("#{cachefile}.sha256", Digest::SHA256.hexdigest(File.read(cachefile)))
+  puts 'Git repo cached.'.lightgreen
 end
