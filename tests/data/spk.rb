37b612434a16f74e66b08c5b2b325cc594caa316
Zopolis4 <creatorsmithmdt@gmail.com>
2024-03-17 10:33:32 +1100

    oh testing ig

---
+++ tests/data/spk.rb
@@ -0,0 +1,11 @@
+require 'buildsystems/autotools'
+
+class OpenAIS < Autotools
+  description 'OpenAIS was an open implementation of the Application Interface Specification (AIS) provided by the Service Availability Forum (SAForum or SA).'
+  homepage 'https://github.com/corosync/openais'
+  version '1.1.4'
+  license 'BSD-3-Clause'
+  compatibility 'all'
+  source_url 'https://github.com/corosync/openais/releases/download/openais-1.1.4/openais-1.1.4.tar.gz'
+  source_sha256 '974b4959f3c401c16156dab31e65a6d45bbf84dd85a88c2a362712e738c06934'
+end
