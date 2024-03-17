92db68b4ea58089a47ce5190cf50b749a3169e37
Zopolis4 <creatorsmithmdt@gmail.com>
2024-03-17 11:16:20 +1100

    binaries shminaries

--- tests/commands/download.rb
+++ tests/commands/download.rb
@@ -10,7 +10,7 @@ class DownloadCommandTest < Minitest::Test
   def test_download_tar
     pkg = Package.load_package(File.join(CREW_LIB_PATH, 'tests/data/openais.rb'))
     filename, extract_dir = Command.download(pkg, nil)
-    assert_equal(File.basename(pkg.url), filename)
+    assert_equal(File.basename(pkg.source_url), filename)
     assert(File.file?(File.join(CREW_BREW_DIR, extract_dir)))
   end
 end
