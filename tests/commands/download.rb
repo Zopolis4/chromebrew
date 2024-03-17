fa2d9691889a594ac705d63774b97ed4553d3a6a
Zopolis4 <creatorsmithmdt@gmail.com>
2024-03-17 11:20:24 +1100

    so much testing

--- tests/commands/download.rb
+++ tests/commands/download.rb
@@ -9,7 +9,13 @@ $LOAD_PATH.unshift File.join(CREW_LIB_PATH, 'lib')
 class DownloadCommandTest < Minitest::Test
   def test_download_tar
     pkg = Package.load_package(File.join(CREW_LIB_PATH, 'tests/data/openais.rb'))
-    filename, extract_dir = Command.download(pkg, nil)
+    expected_output = <<~EOT
+      No precompiled binary available for your platform, downloading source...
+      Openais archive downloaded.
+    EOT
+    assert_output expected_output, nil do
+      filename, extract_dir = Command.download(pkg, nil)
+    end
     assert_equal(File.basename(pkg.source_url), filename)
     assert(File.exist?(File.join(CREW_BREW_DIR, extract_dir)))
   end
