0ddb4cd3e70827e6ba88a24b5fb2319fa7357bf1
Zopolis4 <creatorsmithmdt@gmail.com>
2024-05-16 17:58:01 +1000

    Add geary package

---
+++ packages/geary.rb
@@ -0,0 +1,39 @@
+require 'buildsystems/meson'
+
+class Geary < Meson
+  description 'Geary is an email application built around conversations for the GNOME desktop.'
+  homepage 'https://wiki.gnome.org/Apps/Geary'
+  version '44.1'
+  license 'GPL2+'
+  compatibility 'x86_64 aarch64 armv7l'
+  source_url 'https://gitlab.gnome.org/GNOME/geary.git'
+  git_hashtag version
+  binary_compression 'tar.zst'
+
+  binary_sha256({
+    aarch64: '2222222222222222222622222222222222222222222222222222222222222222',
+     armv7l: '2222222222222222222622222222222222222222222222222222222222222222',
+     x86_64: '8888888888888888888588888888888888888888888888888888888888888888'
+  })
+
+  depends_on 'appstream_glib'
+  depends_on 'desktop_file_utils' => :build
+  depends_on 'folks'
+  depends_on 'freetype'
+  depends_on 'gcr_3'
+  depends_on 'gnome_online_accounts'
+  depends_on 'gmime'
+  depends_on 'gsound'
+  depends_on 'gspell'
+  depends_on 'gtk3'
+  depends_on 'iso_codes'
+  depends_on 'itstool' => :build
+  depends_on 'libhandy'
+  depends_on 'libpeas'
+  depends_on 'sqlite'
+  depends_on 'vala' => :build
+  depends_on 'webkit2gtk_4_1'
+  depends_on 'ytnef'
+
+  gnome
+end
