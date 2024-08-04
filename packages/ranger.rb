205301d5d558f00ce35101ae433521d55d0bfab2
Zopolis4 <creatorsmithmdt@gmail.com>
2024-08-05 09:03:47 +1000

    ranger 1.8.1 -> 1.9.3

--- packages/ranger.rb
+++ packages/ranger.rb
@@ -2,24 +2,26 @@ require 'package'
 
 class Ranger < Package
   description 'A VIM-inspired filemanager for the console.'
-  homepage 'http://ranger.nongnu.org/'
-  version '1.8.1'
+  homepage 'https://ranger.github.io/'
+  version '1.9.3'
   license 'GPL-3'
   compatibility 'all'
-  source_url 'https://github.com/ranger/ranger/archive/v1.8.1.tar.gz'
-  source_sha256 'ab0e32159cde196df4ff14e9c516aaf8ac2db79e3fdee17e59f327d677a96949'
-  binary_compression 'tar.xz'
+  source_url 'https://github.com/ranger/ranger.git'
+  git_hashtag "v#{version}"
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '7e254e34970e1edf6b433e2fafa8bce85597b91000a2cd1e18a1863900732690',
-     armv7l: '7e254e34970e1edf6b433e2fafa8bce85597b91000a2cd1e18a1863900732690',
-       i686: '81df73eb3a29c5e5ec0ff3a4ff0e054dfb5bb9e619e0ffaa3681b84939d3397a',
-     x86_64: '61ded76a93aa9206d72a2618732677990706ffe3666498fe3705a153ec40493e'
+    aarch64: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+     armv7l: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+       i686: 'iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii',
+     x86_64: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
   })
 
   depends_on 'less'
   depends_on 'ncurses'
-  depends_on 'python2'
+  depends_on 'python3'
+  depends_on 'py3_pylint' => :build
+  depends_on 'py3_pytest' => :build
 
   def self.build
     system 'make'
