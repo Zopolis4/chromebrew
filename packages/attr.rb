c758073e2b0260e162cea6089fdd3747122663f4
Zopolis4 <creatorsmithmdt@gmail.com>
2024-08-07 09:49:35 +1000

    attr 2.5.1-1 -> 2.5.2

--- packages/attr.rb
+++ packages/attr.rb
@@ -1,32 +1,24 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Attr < Package
+class Attr < Autotools
   description 'Commands for Manipulating Filesystem Extended Attributes.'
-  homepage 'http://savannah.nongnu.org/projects/attr'
-  version '2.5.1-1'
+  homepage 'https://savannah.nongnu.org/projects/attr'
+  version '2.5.2'
   license 'LGPL-2.1'
   compatibility 'all'
-  source_url 'https://download.savannah.gnu.org/releases/attr/attr-2.5.1.tar.xz'
-  source_sha256 'db448a626f9313a1a970d636767316a8da32aede70518b8050fa0de7947adc32'
+  source_url 'https://git.savannah.gnu.org/git/attr.git'
+  git_hashtag "v#{version}"
   binary_compression 'tar.xz'
 
   binary_sha256({
-    aarch64: '064c27a5daaecabe639c40b1b307bdc7711e7132903d300de699219dc8d00685',
-     armv7l: '064c27a5daaecabe639c40b1b307bdc7711e7132903d300de699219dc8d00685',
-       i686: 'fb434974fc3a48c3122785de09515115762555e9c7551db8734e54f251db6c51',
-     x86_64: '690156378a76ac649c3fd780c517042f74ef812da45e9f84274b2ce5e80b3978'
+    aarch64: '327ef709fcbdae70f8b48a9bb7de07acc9df0013030b01c28639454b732dbe21',
+     armv7l: '327ef709fcbdae70f8b48a9bb7de07acc9df0013030b01c28639454b732dbe21',
+       i686: '85bc6ad138cd6dd706917669bc62ad029214cf29a1f18b9ff8509baf2f8aa785',
+     x86_64: '22a9f8ca21ff2f3ecde4564e73a29005f45bb8de3c125a263129e907f0d1b246'
   })
 
   depends_on 'libcap' => :build
+
   no_zstd
-
-  def self.build
-    system "./configure \
-      #{CREW_OPTIONS}"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  configure_options '--disable-nls'
 end
