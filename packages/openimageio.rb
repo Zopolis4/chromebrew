7220bb47eb6b5e2057a999f01aa83834cf4d24a0
Zopolis4 <creatorsmithmdt@gmail.com>
2024-09-10 21:59:31 +1000

    openimageio 2.5.12.0 -> 2.5.15.0

--- packages/openimageio.rb
+++ packages/openimageio.rb
@@ -3,7 +3,7 @@ require 'buildsystems/cmake'
 class Openimageio < CMake
   description 'OpenImageIO is a toolset for reading, writing, and manipulating image files of any image file format relevant to VFX / animation via a format-agnostic API with a feature set, scalability, and robustness needed for feature film production.'
   homepage 'https://github.com/AcademySoftwareFoundation/OpenImageIO'
-  version '2.5.12.0-py3.12'
+  version '2.5.15.0'
   license 'Apache-2.0'
   compatibility 'x86_64 aarch64 armv7l'
   source_url 'https://github.com/AcademySoftwareFoundation/OpenImageIO.git'
@@ -11,9 +11,9 @@ class Openimageio < CMake
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '293f193c4c9bd02dae17d5b2f6fe68925f804e5a2e67bf1a2344c1fb80df1a2b',
-     armv7l: '293f193c4c9bd02dae17d5b2f6fe68925f804e5a2e67bf1a2344c1fb80df1a2b',
-     x86_64: 'c5bc2ce8a7ff1e45ff7e8c4e1e5a573a61e10d56ed71c929173e9758213da339'
+    aarch64: 'e844c3135cefcb3c202b0771b156bf55ead373095eeede042d8fa229c9c4bc63',
+     armv7l: 'e844c3135cefcb3c202b0771b156bf55ead373095eeede042d8fa229c9c4bc63',
+     x86_64: 'ee3e836bd3f707b04fbb484cd57c07c17a952c661011de67cf69c5d043852320'
   })
 
   depends_on 'boost'
@@ -31,5 +31,5 @@ class Openimageio < CMake
   depends_on 'zlib' # R
 
   # The setup for finding pybind11 is custom, so it's hard to tell where exactly it breaks to fix it properly.
-  cmake_options "-Dpybind11_ROOT=#{CREW_PREFIX}/lib/python#{version.split('y').last}/site-packages/pybind11 -DOIIO_BUILD_TESTS=OFF"
+  cmake_options "-Dpybind11_ROOT=#{CREW_PREFIX}/lib/python3.12/site-packages/pybind11 -DOIIO_BUILD_TESTS=OFF"
 end
